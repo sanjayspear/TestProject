c22a26823b1e99a1e045ce9bd6ae163d5c478ddb
Sanjay Y M <sanjayspear@gmail.com>
2018-04-08 22:43:25 +0530

    Added TestJava.java class file

---
+++ TestProject/src/com/javaPatterns/TestJava.java
@@ -0,0 +1,11 @@
+package com.javaPatterns;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow GitHub ... !");
+
+	}
+
+}
