471b95246fe3c7177ef36692e94e860ec74c52f8
Sanjay Y M <sanjayspear@gmail.com>
2018-04-08 22:47:25 +0530

    Added extra print statement

--- TestProject/src/com/javaPatterns/TestJava.java
+++ TestProject/src/com/javaPatterns/TestJava.java
@@ -5,6 +5,7 @@ public class TestJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hellow GitHub ... !");
+		System.out.println("Added 2nd line");
 
 	}
 
