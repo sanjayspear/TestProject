17c6ec32ff15fcf1e50233a85e94775096c7b8ef
Sanjay Y M <sanjayspear@gmail.com>
2018-04-08 22:59:34 +0530

    Added new print statement

--- TestProject/src/com/javaPatterns/Pattern1.java
+++ TestProject/src/com/javaPatterns/Pattern1.java
@@ -5,6 +5,8 @@ public class Pattern1 {
 	public static void main(String[] args) {
 		// Develop any pattern program below
 		
+		System.out.println("Develop any java pattern below ... ");
+		
 		
 
 	}
